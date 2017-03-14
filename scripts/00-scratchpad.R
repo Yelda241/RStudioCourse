1f921a604e2c0739d8c52d51d48ead97836ca359
YeldaNasifoglu <yelda.nasifoglu@history.ox.ac.uk>
2017-03-14 10:09:58 +0000

    Initial commit

---
+++ scripts/00-scratchpad.R
@@ -0,0 +1,20 @@
+7^2
+sin(30)
+log(1)
+log10(2)
+log2(3)
+
+
+
+1==1 #compare
+1==2 #comparison
+1<=1.2
+1 != 2
+1=1
+
+x <- log(2) #assignment operator, with an error
+x
+x <- 1/30 #this will override previous value of x
+
+
+y <- 1:5 #vectorization
